6c5fcdde51219e741b55226aaef2ff0e31fa7078
Jonas Claesson <jonasc15@student.uia.no>
2019-02-03 16:12:58 +0100

    started experimentation with XML-readers

--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,36 @@
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JFrame;
+import javax.xml.parsers.*;
+import java.io.*;
+import org.w3c.dom.*;
+
 
 public class Game extends JFrame implements KeyListener
 {
-	private Scene[] sceneArray = new Scene[10];//10 is placeholder. Is to be the length of all # of scenes
-	private int i_currentState;//program states: main menu (0), game (1) and pause (2)
+	private Scene[] sceneArray = new Scene[2];//2 is placeholder. Is to be the length of all # of scenes
+	private int i_currentState = 0;//program states: main menu (0), game (1) and pause (2)
+	private int i_currentScene = 0;//i_sceneID for current scene
+	private int i_windowWidth = 1600;
+	private int i_windowHeight = 800;
+	
+	
+	
+	
 	
 	public Game() //initialize
 	{
+		setSize(i_windowWidth, i_windowHeight);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+		setResizable(true);
+		Graphics g = this.getGraphics();
+		g.setColor(Color.cyan);
+		addKeyListener(this);
+		
+		LoadXML();
 		
 		GameLoop();
 	}
@@ -16,6 +38,37 @@ public class Game extends JFrame implements KeyListener
 	
 	public void GameLoop() 
 	{
+		while(true)
+		{
+			while(i_currentState == 0)//main menu
+			{
+				
+			}
+			
+			while(i_currentState == 1)//game
+			{
+				
+			}
+			
+			while(i_currentState == 2)//pause
+			{
+				
+			}
+		}
+	}
+	
+	public void LoadXML()
+	{
+		DocumentBuilderFactory DBF = DocumentBuilderFactory.newInstance();
+		
+		//DOM
+		try {
+			DocumentBuilder DB = DBF.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	}
 	
@@ -31,27 +84,45 @@ public class Game extends JFrame implements KeyListener
 	
 	public void Save()
 	{
-		
+		try
+		{
+			
+		}
+		//catch(){}
+		finally{}
 	}
 	
 	public void Pause()
 	{
+		System.out.println("test pause");
+		
 		if(i_currentState == 1)
 		{
-			
+			i_currentState = 2;
 		}
 		else if(i_currentState == 2)
 		{
-			
+			i_currentState = 1;
 		}
 	}
 
 	@Override
 	public void keyPressed(KeyEvent KE) {
 		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent KE) {
+		// TODO Auto-generated method stub
 		if(KE.getKeyCode() == KeyEvent.VK_SPACE)//TODO continue/scroll text & go to next scene
 		{
+			System.out.println("test space");
 			
+			if(i_currentState==1)
+			{
+				//scroll
+			}
 		}
 		
 		if(KE.getKeyCode() == KeyEvent.VK_ESCAPE)//TODO Pause
@@ -63,12 +134,6 @@ public class Game extends JFrame implements KeyListener
 		{
 			Pause();
 		}
-	}
-
-	@Override
-	public void keyReleased(KeyEvent KE) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
--- src/Scene.java
+++ src/Scene.java
@@ -1,6 +1,10 @@
 
 public class Scene extends GraphicElement
 {
+	
+	//private int[] i_children;
+	private int i_parent;
+	private int i_sceneID;
 
 	public Scene()
 	{

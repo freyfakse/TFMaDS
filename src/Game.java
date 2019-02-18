066b6298793c24dd14783b1db3a904451ce92431
Jonas Claesson <jonasc15@student.uia.no>
2019-02-18 22:20:41 +0100

    added working demo of sax-parser

--- src/Game.java
+++ src/Game.java
@@ -5,19 +5,21 @@ import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 import javax.xml.parsers.*;
 import java.io.*;
-import org.w3c.dom.*;
+import java.util.ArrayList;
 
+import org.xml.sax.Attributes;
+//import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
-public class Game extends JFrame implements KeyListener
-{
-	private int i_numOfScenes; //N.B. has to be properly set before init of 'sceneArray'.
-	private Scene[] sceneArray = new Scene[i_numOfScenes]; //TODO consider use of arraylist, it's also random access.
-	private int i_currentState = 0; //program states: main menu (0), game (1) and pause (2)
-	private int i_currentScene = 0; //i_sceneID for current scene
+public class Game extends JFrame implements KeyListener {
+	private ArrayList<Scene> al_scenes = new ArrayList<Scene>();
+	private int i_currentState = 0; // program states: main menu (0), game (1) and pause (2)
+	private int i_currentScene = 0; // i_sceneID for current scene
 	private int i_windowWidth = 1600;
 	private int i_windowHeight = 800;
-	
-	public Game() //initialize
+
+	public Game() // initialize
 	{
 		setSize(i_windowWidth, i_windowHeight);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -26,110 +28,178 @@ public class Game extends JFrame implements KeyListener
 		Graphics g = this.getGraphics();
 		g.setColor(Color.cyan);
 		addKeyListener(this);
-		
+
 		LoadXML();
-		
+
 		GameLoop();
 	}
-	
-	
+
 	public void GameLoop() //
 	{
-		while(true)
+		while (true) 
 		{
-			while(i_currentState == 0)//main menu
+			while (i_currentState == 0)// main menu
 			{
-				
+
 			}
-			
-			while(i_currentState == 1)//game
+
+			while (i_currentState == 1)// game
 			{
-				
+
 			}
-			
-			while(i_currentState == 2)//pause
+
+			while (i_currentState == 2)// pause
 			{
-				
+
 			}
 		}
 	}
-	
-	public void LoadXML()
+
+	public void LoadXML() 
 	{
-		DocumentBuilderFactory DBF = DocumentBuilderFactory.newInstance();
-		
-		//DOM Method
-		//TODO investigate instead using SAX
-		try {
-			DocumentBuilder DB = DBF.newDocumentBuilder();
-		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
+		try 
+		{
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+			SAXParser saxParser = factory.newSAXParser();
+
+			DefaultHandler handler = new DefaultHandler() 
+			{
+				boolean bfname = false;
+				boolean blname = false;
+				boolean bnname = false;
+				boolean bsalary = false;
+
+				public void startElement(String uri, String localName, String qName, Attributes attributes)
+						throws SAXException 
+				{
+					System.out.println("Start Element :" + qName);
+
+					if (qName.equalsIgnoreCase("FIRSTNAME")) 
+					{
+						bfname = true;
+					}
+
+					if (qName.equalsIgnoreCase("LASTNAME")) 
+					{
+						blname = true;
+					}
+
+					if (qName.equalsIgnoreCase("NICKNAME")) 
+					{
+						bnname = true;
+					}
+
+					if (qName.equalsIgnoreCase("SALARY")) 
+					{
+						bsalary = true;
+					}
+
+				}
+
+				public void endElement(String uri, String localName, String qName) throws SAXException 
+				{
+					System.out.println("End Element :" + qName);
+				}
+
+				public void characters(char ch[], int start, int length) throws SAXException 
+				{
+
+					if (bfname) 
+					{
+						System.out.println("First Name : " + new String(ch, start, length));
+						bfname = false;
+						System.out.println(length);
+					}
+
+					if (blname) 
+					{
+						System.out.println("Last Name : " + new String(ch, start, length));
+						blname = false;
+					}
+
+					if (bnname) 
+					{
+						System.out.println("Nick Name : " + new String(ch, start, length));
+						bnname = false;
+					}
+
+					if (bsalary) 
+					{
+						System.out.println("Salary : " + new String(ch, start, length));
+						bsalary = false;
+					}
+				}
+			};
+
+			saxParser.parse("File.txt", handler);
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	public void Load() //TODO load saved game.
+
+	// }
+
+	// }
+
+	public void Load() // TODO load saved game.
 	{
-		try
-		{
-			
+		try {
+
 		}
-		//catch(){}
-		finally{}
-	}
-	
-	public void Save() //TODO save game to .txt-file. Should just be scene ID.
-	{
-		try
-		{
-			
+		// catch(){}
+		finally {
 		}
-		//catch(){}
-		finally{}
 	}
-	
-	public void SwitchFromOrToPauseState()
+
+	public void Save() // TODO save game to .txt-file. Should just be scene ID.
 	{
+		try {
+
+		}
+		// catch(){}
+		finally {
+		}
+	}
+
+	public void SwitchFromOrToPauseState() {
 		System.out.println("test pause");
-		
-		if(i_currentState == 1)
-		{
+
+		if (i_currentState == 1) {
 			i_currentState = 2;
-			System.out.println("1 "+i_currentState);
-		}
-		else if(i_currentState == 2)
-		{
+			System.out.println("1 " + i_currentState);
+		} else if (i_currentState == 2) {
 			i_currentState = 1;
-			System.out.println("2 "+i_currentState);
+			System.out.println("2 " + i_currentState);
 		}
 	}
 
 	@Override
 	public void keyPressed(KeyEvent KE) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent KE) {
 		// TODO Auto-generated method stub
-		if(KE.getKeyCode() == KeyEvent.VK_SPACE) //TODO continue/scroll text & go to next scene
+		if (KE.getKeyCode() == KeyEvent.VK_SPACE) // TODO continue/scroll text &
+													// go to next scene
 		{
 			System.out.println("test space");
-			
-			if(i_currentState==1)
-			{
-				//scroll
+
+			if (i_currentState == 1) {
+				// scroll
 			}
 		}
-		
-		if(KE.getKeyCode() == KeyEvent.VK_ESCAPE) //TODO Pause
+
+		if (KE.getKeyCode() == KeyEvent.VK_ESCAPE) // TODO Pause
 		{
 			SwitchFromOrToPauseState();
 		}
-		
-		if(KE.getKeyCode() == KeyEvent.VK_P) //TODO also pause
+
+		if (KE.getKeyCode() == KeyEvent.VK_P) // TODO also pause
 		{
 			SwitchFromOrToPauseState();
 		}
@@ -138,6 +208,6 @@ public class Game extends JFrame implements KeyListener
 	@Override
 	public void keyTyped(KeyEvent KE) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
--- src/Scene.java
+++ src/Scene.java
@@ -2,10 +2,11 @@ import java.util.ArrayList;
 
 public class Scene extends GraphicElement
 {
-	private ArrayList<String> arrLi_Text = new ArrayList<String>(); //all dialog and text in current scene
+	//private ArrayList<String> arrLi_Text = new ArrayList<String>(); //all dialog and text in current scene
 	private ArrayList<Integer> arrLi_children = new ArrayList<Integer>(); //position in sceneArray of next scenes
 	private int i_parent; //position in sceneArray of previous scene
 	private int i_sceneID; //position in sceneArray of current scene
+	private String str_text; //all dialog and text in current scene
 
 	public Scene()
 	{
